5bce3e803ec99daed1486e0d090b1c740951b3ed
Fred Pinheiro <fredvpg@gmail.com>
2024-07-04 09:42:34 -0300

    rate limiting

---
+++ api-reservas/Middleware/ClientRateLimiter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Concurrent;
+using System.Threading.RateLimiting;
+
+namespace api_reservas.Middleware
+{
+    public class ClientRateLimiter
+    {
+        private readonly ConcurrentDictionary<string, TokenBucketRateLimiter> _rateLimiters;
+        private readonly TokenBucketRateLimiterOptions _options;
+
+        public ClientRateLimiter(TokenBucketRateLimiterOptions options)
+        {
+            _options = options;
+            _rateLimiters = new ConcurrentDictionary<string, TokenBucketRateLimiter>();
+        }
+
+        public async ValueTask<RateLimitLease> AcquireAsync(string clientIp, int permits)
+        {
+            var rateLimiter = _rateLimiters.GetOrAdd(clientIp, _ => new TokenBucketRateLimiter(_options));
+            return await rateLimiter.AcquireAsync(permits);
+        }
+    }
+}
--- api-reservas/Middleware/RateLimitingMiddleware.cs
+++ api-reservas/Middleware/RateLimitingMiddleware.cs
@@ -5,24 +5,31 @@ namespace api_reservas.Middleware
     public class RateLimitingMiddleware
     {
         private readonly RequestDelegate _next;
-        private readonly TokenBucketRateLimiter _rateLimiter;
+        private readonly ClientRateLimiter _rateLimiter;
 
-        public RateLimitingMiddleware(RequestDelegate next)
+        public RateLimitingMiddleware(RequestDelegate next, ClientRateLimiter rateLimiter)
         {
             _next = next;
-            _rateLimiter = new TokenBucketRateLimiter(new TokenBucketRateLimiterOptions
-            {
-                TokenLimit = 100, // -- Maximum tokens in the bucket
-                TokensPerPeriod = 10, // -- Tokens added per period
-                ReplenishmentPeriod = TimeSpan.FromMinutes(1), // -- Replenishment period
-                AutoReplenishment = true
-            });
+            _rateLimiter = rateLimiter;
         }
+
+        //public RateLimitingMiddleware(RequestDelegate next)
+        //{
+        //    _next = next;
+        //    _rateLimiter = new TokenBucketRateLimiter(new TokenBucketRateLimiterOptions
+        //    {
+        //        TokenLimit = 100, // -- Maximum tokens in the bucket
+        //        TokensPerPeriod = 10, // -- Tokens added per period
+        //        ReplenishmentPeriod = TimeSpan.FromMinutes(1), // -- Replenishment period
+        //        AutoReplenishment = true
+        //    });
+        //}
 
         public async Task InvokeAsync(HttpContext context)
         {
+            var clientIp = context.Connection.RemoteIpAddress?.ToString() ?? "unknown";
             // Acquire one token (one permit) from the rate limiter
-            var result = await _rateLimiter.AcquireAsync(1);
+            var result = await _rateLimiter.AcquireAsync(clientIp, 1);
 
             if (result.IsAcquired)
             {
